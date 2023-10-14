43a9de70e91ec8ec550f4f9c1cc36c7dd263a344
AmorFatiBA <amor.fati291708@gmail.com>
2023-10-14 16:34:02 +0200

    create user router

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRouter from "./routes/user.route.js";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -12,8 +13,11 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRouter);
